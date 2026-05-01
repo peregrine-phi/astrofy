6ef6288e809afa4378476c8121f4e89c91936fee
peregrine-phi <w2228361456@gmail.com>
2026-05-01 16:33:12 +0800

    chore: update site URL to peregrinewang.me in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://peregrinewang.me',
   integrations: [mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: "en",
